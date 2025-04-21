736903e8bdd938603d6cad9c4045f81fe3229f82
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2025-04-21 19:02:18 +0530

    changed to development env

--- Client/next.config.mjs
+++ Client/next.config.mjs
@@ -2,9 +2,15 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
+      {  
         protocol: 'http',
         hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/media/**',
+      },
+      {  
+        protocol: 'http',
+        hostname: 'localhost',
         port: '8000',
         pathname: '/media/**',
       },
