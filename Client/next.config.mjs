f49d94d66ac4173a960e96cf68db41082f9e9776
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2025-05-04 05:55:55 +0530

    docker config for production

--- Client/next.config.mjs
+++ Client/next.config.mjs
@@ -2,24 +2,24 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {  
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
-        pathname: '/media/**',
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/media/**",
       },
-      {  
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/media/**',
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/media/**",
       },
       {
         // Add your production backend domain here
         // http://54.90.145.153/api/lecturer/register/
-        protocol: 'http', // or http if your backend isn't using https
+        protocol: "http", // or http if your backend isn't using https
         hostname: process.env.HOST_ADDRESS,
-        pathname: '/media/**',
+        pathname: "/media/**",
       },
     ],
   },
@@ -27,9 +27,15 @@ const nextConfig = {
     // ⚠️ This will completely ignore TypeScript errors during build
     ignoreBuildErrors: true,
   },
+
   eslint: {
     // Also ignore ESLint errors during build
     ignoreDuringBuilds: true,
+  },
+  output: "export",
+  distDir: ".next",
+  images: {
+    unoptimized: true,
   },
 };
 export default nextConfig;
